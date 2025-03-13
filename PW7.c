e9d7a88df1a036f9a0b40e4ef0bb42fc39a2d2ed
Julien-Quinodoz <julien.quinodoz@gmail.com>
2025-03-13 20:53:19 +0100

    pw7- mise a jour pour 3 2 1 0

--- PW7.c
+++ PW7.c
@@ -1,77 +1,71 @@
-/* *************************************************************************** */
-/*                                                                             */
-/*                                                         :::      ::::::::   */
-/*   PW7.c                                               :+:      :+:    :+:   */
-/*                                                     +:+ +:+         +:+     */
-/*   By  -----------------------------------------  +#+  +:+       +#+         */
-/*                                                 +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/12 14:21:02 by --------           #+#    #+#             */
-/*   Updated: 2025/03/12 14:32:26 by --------          ###   ########.fr       */
-/*                                                                             */
-/* *************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PW14.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquinodo <jquinodo@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 12:58:10 by jquinodo          #+#    #+#             */
+/*   Updated: 2025/03/13 18:10:20 by jquinodo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_res(int *arr, int len,  int *used)
+void powerset(int target,int *arr,int len, int pos, int sum, int *used, int to_print)
 {
 	int i = 0;
-	int first =1;
+	int first = 1;
 
-	while(i < len)
+	if(sum == target && to_print == 1)
 	{
-		if(used[i])
+		while(i < len)
 		{
-			if(first == 1)
-				printf("%d",arr[i]);
-			if(first == 0)
-				printf(" %d",arr[i]);
-			first = 0;
+			if(used[i])
+			{
+				if(first == 1)
+					printf("%d", arr[i]);
+				if(first == 0)
+					printf(" %d", arr[i]);
+				first = 0;
+			}
+			i++;
 		}
-		i++;
+		printf("\n");
+		//return;
 	}
-	printf("\n");
-}
-
-void powerset(int target, int *arr, int len, int pos, int sum, int *used)
-{
-	if(sum == target)
-	{
-		print_res(arr, len, used);
-		return;
-	}
-	if(sum > target || pos >= len)
+	if(sum > target || pos >=len)
 		return;
 
 	used[pos] = 1;
-	powerset(target, arr, len, pos + 1, sum + arr[pos], used);
+	powerset(target, arr,len, pos + 1, sum + arr[pos], used, 1);
 	used[pos] = 0;
-	powerset(target, arr, len, pos + 1, sum, used);
+	powerset(target, arr,len, pos + 1, sum, used, 0);
 }
-
 int main(int ac, char **av)
 {
 	if(ac < 2)
 		return 1;
 
+	int i = 0;
 	int *arr;
 	int *used;
-	int len 	= ac - 2;
-	int i		= 0;
-	int target 	= atoi(av[1]);
+	int len = ac - 2 ;
+	int target = atoi(av[1]);
 
-	arr			= malloc(sizeof(int) * len);
-	used 		= calloc(len, sizeof(int));
+	arr = malloc(sizeof(int) * len);
+	used = calloc(len, sizeof(int));
 
 	while(i < len)
 	{
-		arr[i] 	= atoi(av[i + 2]);
+		arr[i] = atoi(av[i+2]);
 		i++;
 	}
 
-	powerset(target, arr, len, 0, 0, used);
-	free(arr);
+	powerset(target, arr, len, 0, 0, used, 0);
 	free(used);
-
+	free(arr);
 	return 0;
 }
