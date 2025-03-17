051f3861186d582180262f1174f8311d860848b3
Julien-Quinodoz <julien.quinodoz@gmail.com>
2025-03-17 12:10:33 +0100

    Bug OK !

--- PW7.c
+++ PW7.c
@@ -1,51 +1,39 @@
-/* *************************************************************************** */
-/*                                                                             */
-/*                                                         :::      ::::::::   */
-/*   PW7.c                                               :+:      :+:    :+:   */
-/*                                                     +:+ +:+         +:+     */
-/*   By: ------------------------------------------  +#+  +:+       +#+        */
-/*                                                 +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/12 12:58:10 by -----------        #+#    #+#             */
-/*   Updated: 2025/03/17 11:47:33 by ----------        ###   ########.fr       */
-/*                                                                             */
-/* *************************************************************************** */
 
-#include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
-// !! BUG A CONTROLER : QUAND TARGET EST 0 EXEMPLE : a.out 0 -2 2 -5 5  RETOUR AVEC UN \N EN TROP ????
-
 void powerset(int target, int *arr, int len, int pos, int sum, int *used)
 {
-    int i = 0;
-    int first = 1;
+	int i = 0;
+	int first = 1;
 
-    if (pos >= len)
-    {
-        if (sum == target)
-        {
-            while (i < len)
-            {
-                if (used[i])
-                {
-                    if (!first)
-                        printf(" ");
-                    printf("%d", arr[i]);
-                    first = 0;
-                }
-                i++;
-            }
-            printf("\n");
-        }
-        return;
-    }
+	if (pos >= len)
+	{
+		if (sum == target)
+		{
+			while (i < len)
+			{
+				if (used[i])
+				{
+					if (!first)
+						printf(" ");
+					printf("%d", arr[i]);
+					first = 0;
+				}
+				i++;
+			}
+			if (!first)
+			printf("\n");
+		}
+		return;
+	}
 
-    used[pos] = 1;
-    powerset(target, arr, len, pos + 1, sum + arr[pos], used);
+	used[pos] = 1;
+	powerset(target, arr, len, pos + 1, sum + arr[pos], used);
 
-    used[pos] = 0;
-    powerset(target, arr, len, pos + 1, sum, used);
+	 used[pos] = 0;
+	powerset(target, arr, len, pos + 1, sum, used);
 }
 
 int main(int ac, char **av)
