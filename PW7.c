a9b5ccb6bbd76478d1f950b618137cfdce7f7a79
Julien-Quinodoz <julien.quinodoz@gmail.com>
2025-03-17 11:51:07 +0100

    bug a fixer pw7- mise a jour pour 3 2 1 0 target 0 à contrôler

--- PW7.c
+++ PW7.c
@@ -6,7 +6,7 @@
 /*   By: jquinodo <jquinodo@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:58:10 by jquinodo          #+#    #+#             */
-/*   Updated: 2025/03/13 20:57:16 by jquinodo         ###   ########.fr       */
+/*   Updated: 2025/03/17 11:47:33 by jquinodo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,59 +14,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// !!!! BUG A FIXER : a.out 0 -2 2 -5 5  : fonctionne ! mais a.out 0 2 -2 5 -5 BUG !
+// !! BUG A CONTROLER : QUAND TARGET EST 0 EXEMPLE : a.out 0 -2 2 -5 5  RETOUR AVEC UN \N EN TROP ????
 
-void powerset(int target,int *arr,int len, int pos, int sum, int *used, int to_print)
+void powerset(int target, int *arr, int len, int pos, int sum, int *used)
 {
-	int i = 0;
-	int first = 1;
+    int i = 0;
+    int first = 1;
 
-	if(sum == target && to_print == 1)
-	{
-		while(i < len)
-		{
-			if(used[i])
-			{
-				if(first == 1)
-					printf("%d", arr[i]);
-				if(first == 0)
-					printf(" %d", arr[i]);
-				first = 0;
-			}
-			i++;
-		}
-		printf("\n");
-		//return;
-	}
-	if(sum > target || pos >=len)
-		return;
+    if (pos >= len)
+    {
+        if (sum == target)
+        {
+            while (i < len)
+            {
+                if (used[i])
+                {
+                    if (!first)
+                        printf(" ");
+                    printf("%d", arr[i]);
+                    first = 0;
+                }
+                i++;
+            }
+            printf("\n");
+        }
+        return;
+    }
 
-	used[pos] = 1;
-	powerset(target, arr,len, pos + 1, sum + arr[pos], used, 1);
-	used[pos] = 0;
-	powerset(target, arr,len, pos + 1, sum, used, 0);
+    used[pos] = 1;
+    powerset(target, arr, len, pos + 1, sum + arr[pos], used);
+
+    used[pos] = 0;
+    powerset(target, arr, len, pos + 1, sum, used);
 }
+
 int main(int ac, char **av)
 {
 	if(ac < 2)
 		return 1;
-
-	int i = 0;
 	int *arr;
 	int *used;
-	int len = ac - 2 ;
 	int target = atoi(av[1]);
-
+	int len = ac -2;
+	int i = 0;
 	arr = malloc(sizeof(int) * len);
-	used = calloc(len, sizeof(int));
-
+	used = calloc(len , sizeof(int));
 	while(i < len)
 	{
-		arr[i] = atoi(av[i+2]);
+		arr[i] = atoi(av[i + 2]);
 		i++;
 	}
-
-	powerset(target, arr, len, 0, 0, used, 0);
+	powerset(target, arr, len, 0, 0, used);
 	free(used);
 	free(arr);
 	return 0;
